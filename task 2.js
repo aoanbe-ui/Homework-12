05c3ad20f4a994fec359cf30e80c25e9f03d34cd
aoanbe-ui <ao.anbe@gmail.com>
2026-05-14 01:30:34 +0900

    Create task 2.js

---
+++ task 2.js
@@ -0,0 +1,51 @@
+console.log("\n=== CLOSURE WITH REGULAR FUNCTION ===");
+
+function createCounter(start) {
+
+  let count = start;
+
+  return function () {
+
+    console.log("Current count:", count);
+
+    count++;
+
+  };
+
+}
+
+const counter1 = createCounter(5);
+
+counter1();
+
+counter1();
+
+counter1();
+
+// The inner function remembers the value of count
+
+// even after createCounter has finished running.
+
+console.log("\n=== CLOSURE WITH ARROW FUNCTION ===");
+
+function createArrowCounter(start) {
+
+  let count = start;
+
+  return () => {
+
+    console.log("Current count:", count);
+
+    count++;
+
+  };
+
+}
+
+const counter2 = createArrowCounter(10);
+
+counter2();
+
+counter2();
+
+counter2()
